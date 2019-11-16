7bd99d244f3870e4feef5dc0351cc807c7224c76
cafernandezr <cafernandezr@libertadores.edu.co>
2019-11-16 18:54:04 -0500

    Identified person

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 d=$(date +%Y-%m-%d-%hh-%mm-%ss).png
 
-raspistill -o /home/pi/homesecurity/images/$d
+raspistill -o /home/pi/homesecurity/images/$d -w 500 -h 500
 
 git add .
 git commit -m "Identified person"
@@ -11,8 +11,9 @@ url='"https://raw.githubusercontent.com/CristhianFernandez/homesecurity/master/i
 body='{"nombre":"1","description":"1", "foto": '
 end="${body} ${url}${d}\" }"
 	
-echo "$end"
 curl --header "Content-Type: application/json" \
   --request POST \
  --data "$end" \
   http://192.168.1.127:8080/criminal/index
+
+curl http://192.168.1.127:8080/face/verify
